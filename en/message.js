dc4d6b3ef84a34cf02a0960a0c2a61b5ce99b549
LexpDev <lexpdev@proton.me>
2025-05-11 23:30:11 -0300

    mensagem nova

--- en/message.js
+++ en/message.js
@@ -8,6 +8,7 @@ const mensagens = [
     '¯\\_(ツ)_/¯',
     'ᕕ( ᐛ )ᕗ',
     'making websites is hard...',
-    '>··<'
+    '>··<',
+    'Would you. Could you. On a train?'
 ];
 document.getElementById('mensagem').textContent = mensagens[Math.floor(Math.random() * mensagens.length)];
